2a2ead60992c684068fc6b3946416c5399c5083d
José Antonio Yébenes <joseayebenes@gmail.com>
2025-08-01 01:17:05 +0200

    .

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -73,11 +73,22 @@ class MoviedbDatasource extends MoviesDatasource {
   @override
   Future<Movie> getMovieById(String id) async {
     final response = await dio.get('/movie/$id');
-    if (response.statusCode != 200)
+    if (response.statusCode != 200) {
       throw Exception('Movie with id: $id not found');
+    }
 
     final movieDetails = MovieDetails.fromJson(response.data);
     final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
     return movie;
+  }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response = await dio.get(
+      '/search/movie',
+      queryParameters: {'query': query},
+    );
+
+    return _jsonToMovies(response.data);
   }
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,37 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:myapp/domain/entities/movie.dart';
+import 'package:myapp/presentation/delegates/search_movie_delegate.dart';
+import 'package:myapp/presentation/providers/providers.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
     return SafeArea(
       bottom: false,
       child: Padding(
-        padding: const EdgeInsets.symmetric( horizontal: 10 ),
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: SizedBox(
           width: double.infinity,
           child: Row(
             children: [
-              Icon( Icons.movie_outlined, color: colors.primary ),
-              const SizedBox( width: 5 ),
-              Text('Cinemapedia', style: titleStyle ),
-      
+              Icon(Icons.movie_outlined, color: colors.primary),
+              const SizedBox(width: 5),
+              Text('Cinemapedia', style: titleStyle),
+
               const Spacer(),
-      
-              IconButton(onPressed: (){
-      
-              }, 
-              icon: const Icon(Icons.search)
-              )
+
+              IconButton(
+                onPressed: () {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  showSearch<Movie?>(
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      searchMovies: movieRepository.searchMovies,
+                    ),
+                  ).then((movie) {
+                    if (movie == null) return;
+                    context.push('/movie/${movie.id}');
+                  });
+                },
+                icon: const Icon(Icons.search),
+              ),
             ],
           ),
         ),
-      )
+      ),
     );
   }
 }
